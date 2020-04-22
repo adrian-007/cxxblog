9b8ae1f9ddb6e0bab8d16a168e8d44ee73be04a0
Adrian Guzowski <adrian-007@o2.pl>
2020-04-22 16:50:20 +0200

    Fix cmark-gfm repo URL for anonymous access

--- cmake/cmark-gfm.cmake
+++ cmake/cmark-gfm.cmake
@@ -2,7 +2,7 @@ get_directory_property(CURRENT_EP_PREFIX EP_PREFIX)
 set(CMAKR_INSTALL_PREFIX ${CURRENT_EP_PREFIX}/install/cmark_gfm)
 
 ExternalProject_Add(cmark_gfm
-    GIT_REPOSITORY    git@github.com:github/cmark-gfm
+    GIT_REPOSITORY    https://github.com/github/cmark-gfm.git
     GIT_TAG           0.29.0.gfm.0
     CMAKE_CACHE_ARGS
     -DCMARK_TESTS:BOOL=OFF
